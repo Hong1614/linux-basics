a5051e39153259d9922e4560480e15c11be16fcb
Hong1614 <hongjintaek086@gmail.com>
2025-04-09 11:19:46 +0900

    자동 커밋

---
+++ quiz6.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+mkdir newLocation
+touch toBeMoved.txt
+mv toBeMoved.txt newLocation/toBeMoved.txt
+#!/bin/sh
+
+mkdir newLocation
+touch toBeMoved.txt
+mv toBeMoved.txt newLocation/toBeMoved.txt
+
